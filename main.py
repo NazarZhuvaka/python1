48676f6440836ba0f3160e2eafd928a9098f4be9
NazarZhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-10-21 19:09:33 +0300

    First commit

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-# print("Hello")
-# name = input("Enter name")
-#
-# print(f"Hello, {name}")
-# a = 5
-# b = 6
-# print(a + b)
+print("Hello")
+name = input("Enter name")
 
-# c = "I`am student"
-# result = "student" in c
-# print(f"Answer is: {result}")
-#
-# d = "qwertyuiop123124"
-# result1 = "23" in d
-# print(f"Answer is: {result1}")
-#
-# print(d.upper() , c.lower())
-#
-# a = 1_2
-# b = 1_5_0
-# text = "Sum is:"
-# sum1 = a + b
-# print(a , b , f"{text}" , sum1)
+print(f"Hello, {name}")
+a = 5
+b = 6
+print(a + b)
 
-# lines = """
-#     ajdnsajndja the kamdasdmlas the
-#     asd,asldasd sadaskdaskd the
-# """
-#
-# a = lines.splitlines()
-# print(a)
-#
-#
-# print("the" in lines.lower())
+c = "I`am student"
+result = "student" in c
+print(f"Answer is: {result}")
+
+d = "qwertyuiop123124"
+result1 = "23" in d
+print(f"Answer is: {result1}")
+
+print(d.upper() , c.lower())
+
+a = 1_2
+b = 1_5_0
+text = "Sum is:"
+sum1 = a + b
+print(a , b , f"{text}" , sum1)
+
+lines = """
+    ajdnsajndja the kamdasdmlas the
+    asd,asldasd sadaskdaskd the
+"""
+
+a = lines.splitlines()
+print(a)
+
+
+print("the" in lines.lower())
