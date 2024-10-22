2f108143d14248a90f66ada96602b631e905578e
NazarZhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-10-22 16:06:20 +0300

    task

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,47 @@
 # a = True
 # if a: print("Good!")
 
+
 # Оператор pass
 
 # if True:
 #     pass
 # print("Ahoj")
+
+# Test_1
+
+# number = input("write number :")
+# input вертає завжди строку(str) , тому потрбіно його переробити
+# number = input("write number :")
+#
+#
+# if number.isdigit() % 2:
+#     print("Непарне")
+# else:
+#     print("Парне")
+
+number = input("write number :")
+
+if number.isdigit:
+    print("This is num!")
+    num = float(number)
+
+    if num % 2:
+        print("Непарне")
+    elif num == 0:
+        print("Nula")
+    else:
+        print("Парне!")
+else:
+    print("This isn`t num")
+
+
+
+# if number % 2:
+#     print("Непарне")
+# else:
+#     print("Парне")
+
 
 
 
