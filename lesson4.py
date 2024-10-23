33df261d3cfbdd16bb969c74c010af81f73dd78b
NazarZhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-10-23 16:00:02 +0300

    list

--- lesson4.py
+++ lesson4.py
@@ -1,13 +1,40 @@
-a = "Hello world"
-result = ""
+# a = "Hello world"
+# result = ""
+#
+# for char in a:
+#     if char == "o":
+#         result = result + "a"
+#     elif char == "l":
+#         result = result + "e"
+#     else:
+#         result = result + char
+#
+# print(result)
 
-for char in a:
-    if char == "o":
-        result = result + "a"
-    elif char == "l":
-        result = result + "e"
-    else:
-        result = result + char
+# a = "Hello world"
+# result = ""
+# i = 0
+#
+# while i < len(a):
+#     char = a[i]
+#     if char == "o":
+#         result = result + "a"
+#     elif char == "l":
+#         result = result + "e"
+#     else:
+#         result = result + char
+#     i += 1
+#
+# print(result)
 
-print(result)
+# a = 0
+# b = 1
+# c = 0
+#
+# for i in range(100):
+# 	print(c)
+# 	a = b
+# 	b = c
+# 	c = a+b
+
 
---
+++ lesson5.py
@@ -0,0 +1,12 @@
+a = ['v' , 'f' , 'dsfs' , 'sdasdasda']
+#
+# # func = [print, len]
+# # for i in func:
+# #     for k in a:
+# #             i(k)
+#
+# print(a[3])
+#
+# string1 = "asdasd"
+# print(string1[2])
+print(len(a[1]))
